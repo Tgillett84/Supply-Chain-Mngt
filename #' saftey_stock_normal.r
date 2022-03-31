a6091359e1eb53d52cb348c768289f15d196a3a4
Tgillett84 <tamekatlc2011@gmail.com>
2022-03-31 22:22:14 +0900

    Added Code for Inventory Mngt

---
+++ #' saftey_stock_normal.r
@@ -0,0 +1,45 @@
+#' saftey_stock_normal
+#'
+#' Calculating saftey stock based on the cycle service level.
+#'
+#' Calculating saftey stock  based on  the cycle service level in   an assumed normal distribution.
+#'
+#'
+#' @param annualdemand numeric,annual Expected  demand of the SKU .
+#' @param annualstandarddeviation numeric,  standard  deviation of the SKU during season.
+#'
+#' @param  leadtimeinweeks  leadtime in weeks or order.
+#' @param  csl  cycle service level requested
+#' @param na.rm Logical, remove na if TRUE
+#' @importFrom stats dnorm
+#' @importFrom stats lm
+#' @importFrom stats median
+#' @importFrom stats optim
+#' @importFrom stats optimize
+#' @importFrom stats pnorm
+#' @importFrom stats ppois
+#' @importFrom stats predict
+#' @importFrom stats qnorm
+#' @importFrom stats qpois
+#' @importFrom stats sd
+#' @return a dataframe that contains calculations of K the cost per item short metric noting that condition must me less than 1.
+#' @author "haytham omar  email: <haytham@rescaleanalytics.com>"
+#' @note this is the second version of the inventorize package, all the fucntions are  without
+#' any academic contribution from my side, the aim is to facilitate and ease much of the bookkeeping that is endured during stock analysis.
+#' @export
+#' @examples
+#' saftey_stock_normal(annualdemand=8000,annualstandarddeviation=600,
+#' leadtimeinweeks=4,csl=0.92,na.rm=TRUE)
+
+saftey_stock_normal <-
+  function(annualdemand,annualstandarddeviation,leadtimeinweeks,csl,na.rm=TRUE){
+
+    demandleadtime<- annualdemand *leadtimeinweeks/52
+    sigmadl<- annualstandarddeviation* sqrt(leadtimeinweeks/52)
+    safteyfactor<- qnorm(csl)
+    safteystock<-safteyfactor*sigmadl
+    quantityinstock<- demandleadtime+safteystock
+    allpar<- data.frame("demandleadtime"= demandleadtime,"sigmadl"=sigmadl,"safteyfactor"=safteyfactor,"cyclestock+safteystock"=quantityinstock)
+    return(allpar)
+  }
+
---
+++ Inventorize Rcode-1.r
@@ -0,0 +1,13 @@
+#' inventorize: Inventory Analytics And Cost Calculations.
+#'
+#' inventory analytics,revenue management and cost calculations for SKUs.
+#'
+#' @docType package
+#' @name inventorize
+"_PACKAGE"
+
+# Update this function call
+utils::globalVariables(c("category","%>%","arrange","mutate","desc","aes","xlab","geom_bar","ggplot","theme_minimal","product_mix","ldply",
+                         "ggplotly","Product_name","Measure","Value","revenue","profit","predicted_linear","predicted_logit",
+                         "revenue_linear","revenue_logit","lm_p","logit_p","dplyr::filter","dplyr::lag"))
+
